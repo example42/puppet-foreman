4ee72abdab91fe37d36db1db33c7240add67148c
Dolf Schimmel (Freeaqingme) <dolfschimmel@gmail.com>
2013-04-07 21:09:45 +0200

    vhost and firewalling for webinterface

--- manifests/passenger.pp
+++ manifests/passenger.pp
@@ -15,11 +15,38 @@ class foreman::passenger {
   }
 
   apache::vhost { 'foreman':
-    port     => '80',
-    priority => '20',
-    docroot  => "${foreman::basedir}/public/",
-    ssl      => true,
-    template => $foreman::manage_file_passenger_path,
+    name           => $foreman::vhost_servername,
+    serveraliases  => $foreman::vhost_aliases,
+    port           => '80',
+    priority       => '20',
+    docroot        => "${foreman::basedir}/public/",
+    ssl            => true,
+    template       => $foreman::manage_file_passenger_path,
+  }
+
+  ### Firewall management, if enabled ( firewall => true )
+  if $foreman::bool_firewall == true {
+    firewall { 'foreman_http_80':
+      source      => $foreman::firewall_src,
+      destination => $foreman::firewall_dst,
+      protocol    => 'tcp',
+      port        => 80,
+      action      => 'allow',
+      direction   => 'input',
+      tool        => $foreman::firewall_tool,
+      enable      => $foreman::manage_firewall,
+    }
+
+    firewall { 'foreman_http_443':
+      source      => $foreman::firewall_src,
+      destination => $foreman::firewall_dst,
+      protocol    => 'tcp',
+      port        => 443,
+      action      => 'allow',
+      direction   => 'input',
+      tool        => $foreman::firewall_tool,
+      enable      => $foreman::manage_firewall,
+    }
   }
 
 }
