81835185c1412acaa5d7db97dfabc38717ef4087
Alessandro Franceschi <al@lab42.it>
2013-07-24 00:41:55 +0200

    Permissions on autosign.conf that dont conflict with puppet master

--- manifests/proxy/puppetca.pp
+++ manifests/proxy/puppetca.pp
@@ -2,8 +2,8 @@ class foreman::proxy::puppetca {
   # This file has to be writable by the proxy to support modifying the autosign rules while installing the system
   file { "/etc/puppet/autosign.conf":
     ensure  => present,
-    mode    => 0664,
-    owner   => 'root',
+    mode    => 0644,
+    owner   => $foreman::proxy_user,
     group   => $foreman::proxy_group,
     require => Package['foreman-proxy'];
   }
